55b7257d60b0807e2dcce9d2a4f272d53e22dd24
adonoghuTcd <adonoghu@tcd.ie>
2015-07-12 20:17:00 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is just a litte project that we were working on
+
+
---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp =        require('gulp'),
+    sass =        require('gulp-sass'),
+    minify =      require('gulp-minify-css'),
+    data =        require('gulp-data'),
+    rename =      require('gulp-rename'),
+    fs =          require('fs'),
+    prefixer =    require('gulp-autoprefixer')
+
+
+gulp.task('css', function() {
+  gulp.src('stylesheets/master.scss')
+      .pipe(sass())
+      .pipe(prefixer())
+      .pipe(minify({ cache: true }))
+      .pipe(gulp.dest('./css'))
+})
+
+gulp.task('watch', function() {
+  gulp.watch('./stylesheets/*.scss', ['css'])
+})
+
+gulp.task('default', ['css'])
