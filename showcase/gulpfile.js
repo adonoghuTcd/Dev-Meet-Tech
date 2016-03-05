3dc9e4272a0c2d93c9fae88d56c7ddc02d1e554c
eoghanmartin <martine4@tcd.ie>
2016-03-05 19:12:33 +0000

    LaunchPad event basic setup

--- showcase/gulpfile.js
+++ showcase/gulpfile.js
@@ -3,18 +3,40 @@ var gulp =        require('gulp'),
     minify =      require('gulp-minify-css'),
     rename =      require('gulp-rename'),
     fs =          require('fs'),
-    prefixer =    require('gulp-autoprefixer')
-
-gulp.task('css', function() {
-  gulp.src('stylesheets/master.scss')
-      .pipe(sass())
-      .pipe(prefixer())
-      .pipe(minify({ cache: true }))
-      .pipe(gulp.dest('./css'))
-})
+    prefixer =    require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create();
 
 gulp.task('watch', function() {
   gulp.watch('./stylesheets/*.scss', ['css'])
 })
+gulp.task('watch', ['css'], function(gulpCallback) {
+  browserSync.init({
+    // serve out of app/
+    server: './',
+    // launch default browser as soon as server is up
+    open: true
+  }, function callback() {
 
-gulp.task('default', ['css'])
+    gulp.watch('/launchpad.html', browserSync.reload);
+    gulp.watch('./stylesheets/*.scss', ['css']);
+
+    gulpCallback();
+  });
+});
+
+gulp.task('css', function() {
+  gulp.src('stylesheets/master-pilot.scss')
+      .pipe(sass())
+      .pipe(prefixer())
+      .pipe(minify({ cache: true }))
+      .pipe(gulp.dest('./css'))
+      .pipe(browserSync.stream());
+  gulp.src('stylesheets/master-launchpad.scss')
+      .pipe(sass())
+      .pipe(prefixer())
+      .pipe(minify({ cache: true }))
+      .pipe(gulp.dest('./css'))
+      .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['watch'])
